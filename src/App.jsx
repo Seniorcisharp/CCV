49d9d2ec0fa9e3485915377f9d72437fdd5c6cfc
Seniorcisharp <158044591+Seniorcisharp@users.noreply.github.com>
2024-12-21 16:51:47 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
       <header className="header">
         <div className="header-left">
           <img
-            src="src/img/4.jpg" 
+            src="/images/4.jpg"
             alt="Profile"
             className="profile-img"
           />
